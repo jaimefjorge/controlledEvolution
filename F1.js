3121b9d65fd275cd5e00112597e3f4414351a3a4
Jaime Jorge <Jaime Jorge>
2013-05-24 00:31:59 +0100

    LS/ES/U INC

--- F1.js
+++ F1.js
@@ -19,7 +19,7 @@
       if (infoBox[length] !== 0) {
         if (infoBox[length] <= infoBox[index]) {
           nextTickQueue = [];
-          infoBox[length] = 0;
+          infoBox[length] = 2;
         } else {
           nextTickQueue.splice(0, infoBox[index]);
           infoBox[length] = nextTickQueue.length;
