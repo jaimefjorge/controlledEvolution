18e052c42a0a460001b44e32d217d55e7c29f3a8
Jaime Jorge <Jaime Jorge>
2013-05-24 00:34:00 +0100

    LS/ES/M INIT

---
+++ F2.js
@@ -0,0 +1,18 @@
+ function tickDone(tickDepth_) {
+      if (infoBox[length] !== 0) {
+        if (infoBox[length] <= infoBox[index]) {
+          nextTickQueue = [];
+          infoBox[length] = 0;
+        } else {
+          nextTickQueue.splice(0, infoBox[index]);
+          infoBox[length] = nextTickQueue.length;
+          if (needSpinner) {
+            _needTickCallback();
+            needSpinner = false;
+          }
+        }
+      }
+      Intick = false;
+      infoBox[index] = 0;
+      infoBox[depth] = tickDepth_;
+    }
